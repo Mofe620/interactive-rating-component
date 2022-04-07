a9766c97a1869889ae802ea2ea8eff309c5f92a8
Mofe <moflux620@gmail.com>
2022-04-07 10:34:07 +0100

    C'est finis

---
+++ script.js
@@ -0,0 +1,26 @@
+var btn = document.getElementById("submit");
+var card = document.getElementById("card");
+var container = document.getElementById("container");
+var thankYou = document.getElementById("thankYou");
+thankYou.style.display = 'none';
+var scores = document.getElementsByClassName("score"); //creates a HTMLCollection
+    //convert HTMLCollection to Array
+    var ratings = [].slice.call(scores);
+    ratings.forEach(element => {
+        element.addEventListener("click", function(){
+            element.setAttribute("id", "select");
+            let other = ratings.filter(function(value){
+                return value != element;
+            })
+            other.forEach(item => {
+                item.setAttribute("id", "");
+            })
+
+            btn.addEventListener("click", function(){
+                container.remove();
+                thankYou.style.display = 'block';
+                card.appendChild(thankYou);
+            })
+        })
+
+    })  
